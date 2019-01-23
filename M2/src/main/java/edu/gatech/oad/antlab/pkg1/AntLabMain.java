b1a353ea4244d6af2c73fd10abd134c738cd0984
James Wood <woodjamesdee@gmail.com>
2019-01-23 12:20:07 -0500

    Fixed formatting issues that arose when pulling.

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -52,25 +52,25 @@ import edu.gatech.oad.antlab.pkg2.*;
      */
     public void printOutMessage() {
         
-        String toPrint = 
+        String toPrint =
             ant11.getMessage() + ant12.getMessage() + ant21.getMessage()
           + ant22.getMessage() + ant31.getMessage();
-		  //Person1 replace P1 with your name
-		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("Fuji");
-		  toPrint += p1.toString("acfujimoto3");
-		  //Person2 replace P2 with your name
-		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-          //Person4 replace P4 with your name
-          //and gburdell4 with your gt id
-          Person4 p4 = new Person4("James Wood");
-          toPrint += p4.toString("jwood78");
+                  //Person1 replace P1 with your name
+		          //and gburdell1 with your gt id
+		          Person1 p1 = new Person1("Fuji");
+		          toPrint += p1.toString("acfujimoto3");
+		          //Person2 replace P2 with your name
+		          //and gburdell with your gt id
+		          Person2 p2 = new Person2("P2");
+		          toPrint += p2.toString("gburdell2");
+		          //Person3 replace P3 with your name
+		          //and gburdell3 with your gt id
+                  Person3 p3 = new Person3("Juliet");
+		          toPrint += p3.toString("jbaldwin40");
+                  //Person4 replace P4 with your name
+                  //and gburdell4 with your gt id
+                  Person4 p4 = new Person4("James Wood");
+                  toPrint += p4.toString("jwood78");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
